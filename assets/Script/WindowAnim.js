021b9a7ffbf528e85c1f1bbc412638871b10b1e5
kingBook <412126604@qq.com>
2019-06-04 21:47:18 +0800

    update 2019-06-04 21:47

---
+++ assets/Script/WindowAnim.js
@@ -0,0 +1,50 @@
+cc.Class({
+    extends:cc.Component,
+
+    properties:{
+       
+    },
+    
+    onLoad:function(){
+        var buttonNode=this.getChildByName("button");
+        buttonNode.opacity=0;
+    },
+    
+    rightNowHideAnimal:function(){
+        
+    },
+    
+    otherWindowsComeBack:function(){
+    },
+    
+    /**点击气球*/
+   // onTouchBalloon:function(numNO){
+    
+     /**伸出去*/
+     goOutside:function(numNO){
+         
+     },
+     
+     /**缩回来*/
+    comeback:function(){
+        
+    },
+
+    /**是否伸出*/
+    getIsOut:function(){
+         
+    },
+
+    getIsBackOrOuting:function(){
+        
+    },
+
+    /**窗口显示的数字*/
+    getNumNO:function(){
+       
+    },
+     
+     
+    
+    
+});
